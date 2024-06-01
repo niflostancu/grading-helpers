6d9edbd057eb04be7fe576b8248f73d62c1fd433
Stancu Florin <niflostancu@gmail.com>
2024-06-01 22:18:02 +0300

    activate.sh: cd alias fix + better path var naming

--- activate.sh
+++ activate.sh
@@ -8,29 +8,30 @@ if [ -z "$BASH" ] && [ -z "$ZSH_NAME" ]; then
     exit 1
 fi
 
-if [ -n "$BASH" ]; then
-    SCRIPTS_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE}" )" &> /dev/null && pwd )"
+if [[ -n "$GRADING_SCRIPTS_DIR" ]]; then
+    true
+elif [ -n "$BASH" ]; then
+    GRADING_SCRIPTS_DIR="$( builtin cd -- "$( dirname -- "${BASH_SOURCE}" )" &> /dev/null && pwd )"
 elif [ -n "$ZSH_NAME" ]; then
-    SCRIPTS_DIR="$( cd -- "$( dirname -- "${(%):-%x}" )" &> /dev/null && pwd )"
+    GRADING_SCRIPTS_DIR="$( builtin cd -- "$( dirname -- "${(%):-%x}" )" &> /dev/null && pwd )"
 fi
-SCRIPTS_DIR="$SCRIPTS_DIR"
 
-export _GRADING_SCRIPTS_DIR="$SCRIPTS_DIR"
-export PATH="$SCRIPTS_DIR/bin:$SCRIPTS_DIR/moodle:$SCRIPTS_DIR/gsheet:$PATH"
+export _GRADING_SCRIPTS_DIR="$GRADING_SCRIPTS_DIR"
+export PATH="$GRADING_SCRIPTS_DIR/bin:$GRADING_SCRIPTS_DIR/moodle:$GRADING_SCRIPTS_DIR/gsheet:$PATH"
 
-if [[ -f "$SCRIPTS_DIR/grading.config.sh" ]]; then
+if [[ -f "$GRADING_SCRIPTS_DIR/grading.config.sh" ]]; then
     # load local grading config file (for customizations)
-    source "$SCRIPTS_DIR/grading.config.sh" || return 1
+    source "$GRADING_SCRIPTS_DIR/grading.config.sh" || return 1
 fi
 
 if [[ -z "$GRADING_NO_VENV" ]]; then
-    ( cd "$SCRIPTS_DIR"; make ) || return 1
-    source "$SCRIPTS_DIR/.venv/bin/activate" || return 1
+    ( cd "$GRADING_SCRIPTS_DIR"; make ) || return 1
+    source "$GRADING_SCRIPTS_DIR/.venv/bin/activate" || return 1
     echo "VirtualEnv loaded ($(python3 --version))!"
 fi
 
 # print the path if debugging is active
 if [[ -n "$DEBUG" ]]; then
-    echo "Loaded grading scripts from path: '$SCRIPTS_DIR'"
+    echo "Loaded grading scripts from path: '$GRADING_SCRIPTS_DIR'"
 fi
 
